adb5b331052e67e97a872554ffa378a3577e6b3f
suer <suetsugu.r@gmail.com>
2014-01-29 20:22:38 +0900

    bundle exec rails new . --skip-test-unit --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GhstreaksService::Application.config.secret_key_base = '8ae62e4527f8012db0ad6b07d0ef1e00dc281319e92a77952a1fc79f2e0df57e89a9b869429e0de4d2377b9487f1e94c9744d8832f71ef8f2ca7d7c44eec32ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GhstreaksService::Application.config.session_store :cookie_store, key: '_ghstreaks-service_session'
