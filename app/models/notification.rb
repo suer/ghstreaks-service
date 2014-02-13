30959a318a200f22550f32b3d2c6729e1050e2f3
suer <suetsugu.r@gmail.com>
2014-02-13 17:58:45 +0900

    check device token

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,7 +5,7 @@ class Notification < ActiveRecord::Base
     params[:hour]   ||= 17
     params[:minute] ||= 0
     if user.notifications.exists?
-      Notification.delete_all(user_id: user.id)
+      Notification.delete_all(user_id: user.id, device_token: params[:device_token])
     end
     notification = Notification.create(params)
     user.notifications << notification
