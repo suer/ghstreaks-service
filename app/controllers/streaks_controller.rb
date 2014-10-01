a75533c0b93f3b5076602b5cfe775d95066a3863
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-10-01 22:33:59 +0900

    Changed: follow github contributions page stracture

--- app/controllers/streaks_controller.rb
+++ app/controllers/streaks_controller.rb
@@ -8,9 +8,9 @@ class StreaksController < ApplicationController
     hash = {current_streaks: 0}
     url = "https://github.com/#{username}"
     html = Nokogiri::HTML(Faraday.get(url).body)
-    node = html.css(".contrib-streak-current > .num")
-    return hash if node.empty?
-    hash[:current_streaks] = node.text.split.first.to_i
+    nodes = html.css(".contrib-column > .contrib-number")
+    return hash if nodes.empty?
+    hash[:current_streaks] = nodes.last.text.split.first.to_i
     hash
   rescue
     {current_streaks: 0}
